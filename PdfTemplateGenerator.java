3dba4fda4e7c984974bad424c5dc5f5e92a3e902
Rajesh Kannan K <krajesheeebe@gmail.com>
2025-03-28 20:18:21 +0530

    Update PdfTemplateGenerator.java

--- PdfTemplateGenerator.java
+++ PdfTemplateGenerator.java
@@ -1,5 +1,6 @@
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -35,12 +36,18 @@ public class RestTemplateConfig {
                 .loadTrustMaterial(trustStore, null) // 🔹 For trusting server certificates
                 .build();
 
-        // Create HttpClient with SSL
+        // Use Connection Manager with SSL
+        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
+        connManager.setDefaultSocketConfig(org.apache.hc.core5.util.Timeout.ofMinutes(1));
+
+        // Build HttpClient with SSL
         CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connManager)
+                .setConnectionManagerShared(true)
                 .setSSLContext(sslContext)
                 .build();
 
-        // ✅ Use HttpComponentsClientHttpRequestFactory(HttpClient5)
+        // ✅ Correct constructor for HttpClient 5.x
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
         return new RestTemplate(factory);
     }
