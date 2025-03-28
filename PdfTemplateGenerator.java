b767694df0ed822a97e4dbc3ec69593ad7af7406
Rajesh Kannan K <krajesheeebe@gmail.com>
2025-03-28 21:09:11 +0530

    Update PdfTemplateGenerator.java

--- PdfTemplateGenerator.java
+++ PdfTemplateGenerator.java
@@ -1,9 +1,9 @@
 import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.SocketConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.core5.ssl.SSLContextBuilder;
-import org.apache.hc.core5.util.TimeValue;
 import org.apache.hc.core5.util.Timeout;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -29,19 +29,4 @@ public class RestTemplateConfig {
 
         // ✅ 2. Load Truststore (Server CA Certificates)
         KeyStore trustStore = KeyStore.getInstance("JKS");
-        try (FileInputStream trustStoreStream = new FileInputStream(truststorePath)) {
-            trustStore.load(trustStoreStream, truststorePassword.toCharArray());
-        }
-
-        // ✅ 3. Build SSL Context with Keystore & Truststore
-        SSLContext sslContext = SSLContextBuilder.create()
-                .loadKeyMaterial(keyStore, keystorePassword.toCharArray()) // 🔹 For client authentication
-                .loadTrustMaterial(trustStore, null) // 🔹 For trusting server certificates
-                .build();
-
-        // ✅ 4. Configure Connection Manager
-        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
-        connManager.setDefaultConnectionConfig(
-                ConnectionConfig.custom()
-                        .setSocketTimeout(Timeout.ofSeconds(30)) // Socket read timeout
-                        .setTimeToLive(TimeValue.ofMinutes(5)) // Connection TTL
+        try (FileInputStream trustStoreStream = new FileInput
